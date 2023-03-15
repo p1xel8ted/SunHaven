d0657566b37437afa92ec14d86ff22aef15b1d2c
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-03-15 23:27:54 +1100

    NoTimeForFishing

---
+++ NoTimeForFishing/Patches.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using DG.Tweening;
+using HarmonyLib;
+using UnityEngine.Events;
+using Wish;
+
+namespace NoTimeForFishing;
+
+[HarmonyPatch]
+public static class Patches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(DialogueController), nameof(DialogueController.PushDialogue))]
+    public static bool PushDialogue(ref DialogueController __instance, ref DialogueNode dialogue, ref UnityAction onComplete, ref bool animateOnComplete, ref bool ignoreDialogueOnGoing)
+    {
+        if (!Plugin.DisableCaughtFishWindow.Value) return true;
+
+        if (dialogue.dialogueText.Any(line => line.ToLowerInvariant().Contains("caught")))
+        {
+            onComplete.Invoke();
+            return false;
+        }
+
+        return true;
+    }
+
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(FishingRod), nameof(FishingRod.UseDown1))]
+    public static bool FishingRod_UseDown1(ref FishingRod __instance)
+    {
+        if (!Plugin.SkipFishingMiniGame.Value) return true;
+        if (!__instance.player.IsOwner || !__instance._canUseFishingRod || __instance.Reeling)
+        {
+            return true;
+        }
+        if (__instance._fishing)
+        {
+            if (__instance.ReadyForFish)
+            {
+                if (!__instance._bobber.MiniGameInProgress)
+                {
+                    __instance.wonMiniGame = true;
+                    __instance._frameRate = 8f;
+                    __instance.ReadyForFish = false;
+                    __instance.Reeling = true;
+                    __instance._fishing = !__instance._fishing;
+                    __instance._swingAnimation = (__instance._fishing ? SwingAnimation.VerticalSlash : SwingAnimation.Pull);
+                    var rod = __instance;
+                    DOVirtual.DelayedCall(__instance.ActionDelay / __instance.AttackSpeed(), delegate
+                    {
+                        rod.Action(rod.pos);
+                        rod.SendFishingState(3);
+                        rod.CancelFishingAnimation();
+                    }, false);
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ NoTimeForFishing/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace NoTimeForFishing
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public  class Plugin : BaseUnityPlugin
+    {
+        private const string PluginGuid = "p1xel8ted.sunhaven.notimeforfishing";
+        private const string PluginName = "No Time For Fishing!";
+        private const string PluginVersion = "0.0.1";
+
+        public static ConfigEntry<bool> DisableCaughtFishWindow;
+        public static ConfigEntry<bool> SkipFishingMiniGame;
+
+        private static ManualLogSource LOG { get; set; }
+
+        private void Awake()
+        {
+            LOG = new ManualLogSource("Log");
+            BepInEx.Logging.Logger.Sources.Add(LOG);
+
+            SkipFishingMiniGame = Config.Bind("Fishing", "Skip Fishing Mini Game", true, "Skip the fishing mini game.");
+            DisableCaughtFishWindow = Config.Bind("Caught Fish Window", "Disable Caught Fish Window", true, "Disable the caught fish window.");
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginGuid);
+            LOG.LogWarning($"Plugin {PluginName} is loaded!");
+        }
+
+        private void OnDestroy()
+        {
+            LOG.LogError($"I've been destroyed!");
+        }
+
+        private void OnDisable()
+        {
+            LOG.LogError($"I've been disabled!");
+        }
+    }
+}
