b04880bf84e997845c767af972a8bdefc048c619
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-10-22 10:28:57 +1100

    .

--- AutoTools/Patches.cs
+++ AutoTools/Patches.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using HarmonyLib;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Wish;
 
 namespace AutoTools;
@@ -18,13 +17,6 @@ public static class Patches
     private const string WateringCan = "_watering_can";
     private const string Sword = "_sword";
     private const string Hoe = "_hoe";
-    private static int PickaxeIndex { get; set; } = -1;
-    private static int AxeIndex { get; set; } = -1;
-    private static int ScytheIndex { get; set; } = -1;
-    private static int FishingRodIndex { get; set; } = -1;
-    private static int WateringCanIndex { get; set; } = -1;
-    private static int SwordIndex { get; set; } = -1;
-    private static int HoeIndex { get; set; } = -1;
 
 
     private static readonly Dictionary<string, Action<int>> ToolIndexUpdater = new()
@@ -38,21 +30,41 @@ public static class Patches
         {Hoe, index => HoeIndex = index}
     };
 
+    private static int PickaxeIndex { get; set; } = -1;
+    private static int AxeIndex { get; set; } = -1;
+    private static int ScytheIndex { get; set; } = -1;
+    private static int FishingRodIndex { get; set; } = -1;
+    private static int WateringCanIndex { get; set; } = -1;
+    private static int SwordIndex { get; set; } = -1;
+    private static int HoeIndex { get; set; } = -1;
+
+    private static Dictionary<int, ToolData> ToolIndexToItemMap { get; } = new();
+
+    private static ToolData GetToolDataByToolIndex(int toolIndex)
+    {
+        ToolIndexToItemMap.TryGetValue(toolIndex, out var value);
+        return value;
+    }
+
+
     internal static void UpdateToolIndexes()
     {
-        if (Player.Instance is null) return;
-        if (Player.Instance.PlayerInventory is null) return;
-
+        if (Player.Instance is null || Player.Instance.PlayerInventory is null)
+            return;
+        
         foreach (var tool in ToolIndexUpdater.Keys)
         {
-            ToolIndexUpdater[tool](-1); // Reset all indices
+            ToolIndexUpdater[tool](-1);
         }
+
+        ToolIndexToItemMap.Clear();
 
         foreach (var item in Player.Instance.PlayerInventory._actionBarIcons)
         {
             foreach (var kvp in ToolIndexUpdater.Where(kvp => item.ItemImage is not null && item.ItemImage._image.sprite.name.Contains(kvp.Key)))
             {
-                kvp.Value(item.ItemImage.slotIndex);
+                kvp.Value(item.ItemImage.slotIndex); // Update the tool's index
+                ToolIndexToItemMap[item.ItemImage.slotIndex] = ItemDatabase.GetItemData(item.ItemImage.item) as ToolData; // Map the tool's index to the item's index
                 break;
             }
         }
@@ -79,10 +91,15 @@ public static class Patches
 
 
         if (__instance is null || collider is null || SceneSettingsManager.Instance is null || TileManager.Instance is null) return;
-        //var scene = SceneSettingsManager.Instance.sceneDictionary.FirstOrDefault(a => a.Value.sceneName == SceneManager.GetActiveScene().name);
+
+        if (Plugin.EnableDebug.Value)
+        {
+            Plugin.LOG.LogWarning($"PlayerInteractions_OnTriggerEnter2D: collider name: {collider.name}");
+        }
+
+
         if (!Plugin.EnableAutoToolOnFarmTiles.Value && TileManager.Instance.HasFarmingTile(Player.Instance.Position, ScenePortalManager.ActiveSceneIndex))
         {
-            Plugin.LOG.LogWarning($"PlayerInteractions_OnTriggerEnter2D: Player is on a farming tile!");
             return;
         }
 
@@ -91,7 +108,7 @@ public static class Patches
         var npc = collider.GetComponent<NPCAI>();
         var isEnemy = enemy is not null && npc is null;
         var rock = collider.GetComponent<Rock>();
-        var tree = collider.GetComponent<Wish.Tree>();
+        var tree = collider.GetComponent<Tree>();
         var crop = collider.GetComponent<Crop>();
         var wood = collider.GetComponent<Wood>();
         var plant = collider.GetComponent<Plant>();
@@ -102,31 +119,97 @@ public static class Patches
             UpdateToolIndexes();
         }
 
-        if (isEnemy)
-        {
-            if (Plugin.EnableAutoSword.Value)
-            {
-                HandleToolInteraction(SwordIndex, "No sword on action bar!");
-            }
-        }
+        // if (isEnemy)
+        // {
+            ToolAction(SwordIndex, isEnemy, Plugin.EnableAutoSword.Value, "No sword on action bar!");
+            
+        //     if (Plugin.EnableAutoSword.Value)
+        //     {
+        //         var swordItemData = GetToolDataByToolIndex(SwordIndex);
+        //         if (swordItemData is not null)
+        //         {
+        //             if (CanUse(swordItemData))
+        //             {
+        //                 HandleToolInteraction(SwordIndex, "No sword on action bar!");   
+        //             }
+        //             else
+        //             {
+        //                 Notify($"Your {swordItemData.profession} level is too low to use {swordItemData.name}!",true);
+        //             }
+        //         }
+        //         else
+        //         {
+        //             Notify("Sword is null!", true); 
+        //         }
+        //
+        //        
+        //     }
+        // }
         else if (rock && rock.Pickaxeable)
         {
             if (Plugin.EnableAutoPickaxe.Value)
             {
-                HandleToolInteraction(PickaxeIndex, "No pickaxe on action bar!");
+                var pickAxeToolData = GetToolDataByToolIndex(PickaxeIndex);
+                if (pickAxeToolData is not null)
+                {
+                    if (CanUse(pickAxeToolData))
+                    {
+                        HandleToolInteraction(PickaxeIndex, "No pickaxe on action bar!");   
+                    }
+                    else
+                    {
+                        Notify($"Your {pickAxeToolData.profession} level is too low to use {pickAxeToolData.name}!",true);
+                    }
+                }
+                else
+                {
+                    Notify("PickAxe is null!", true); 
+                }
             }
         }
         else if ((tree && tree.Axeable) || (wood && wood.Axeable))
         {
-            if(Plugin.EnableAutoAxe.Value){
-                HandleToolInteraction(AxeIndex, "No axe on action bar!");
+            if (Plugin.EnableAutoAxe.Value)
+            {
+                var axeToolData = GetToolDataByToolIndex(AxeIndex);
+                if (axeToolData is not null)
+                {
+                    if (CanUse(axeToolData))
+                    {
+                        HandleToolInteraction(AxeIndex, "No axe on action bar!");   
+                    }
+                    else
+                    {
+                        Notify($"Your {axeToolData.profession} level is too low to use {axeToolData.name}!",true);
+                    }
+                }
+                else
+                {
+                    Notify("Axe is null!", true); 
+                }
             }
         }
         else if (plant is not null || (collider.name.Contains("foliage") && !collider.name.Contains("prop")))
         {
             if (Plugin.EnableAutoScythe.Value)
             {
-                HandleToolInteraction(ScytheIndex, "No scythe on action bar!");
+                var scytheToolData = GetToolDataByToolIndex(ScytheIndex);
+                if (scytheToolData is not null)
+                {
+                    if (CanUse(scytheToolData))
+                    {
+                        HandleToolInteraction(ScytheIndex, "No scythe on action bar!");   
+                    }
+                    else
+                    {
+                        Notify($"Your {scytheToolData.profession} level is too low to use {scytheToolData.name}!",true);
+                    }
+                }
+                else
+                {
+                    Notify("Scythe is null!", true); 
+                }
+                
             }
         }
         else if (crop is not null)
@@ -136,31 +219,106 @@ public static class Patches
             {
                 if (Plugin.EnableAutoWateringCan.Value)
                 {
-                    HandleToolInteraction(WateringCanIndex, "No watering can on action bar!");
+                    var wateringCanToolData = GetToolDataByToolIndex(WateringCanIndex);
+                    if (wateringCanToolData.GetItem() is WateringCanItem wc)
+                    {
+                        if(wc.WaterAmount > 0)
+                        {
+                            if (CanUse(wateringCanToolData))
+                            {
+                                HandleToolInteraction(WateringCanIndex, "No watering can on action bar!");   
+                            }
+                            else
+                            {
+                                Notify($"Your {wateringCanToolData.profession} level is too low to use {wateringCanToolData.name}!",true);
+                            }
+                        }
+                        else
+                        {
+                            Notify($"Your {wateringCanToolData.name} is empty!",true);
+                        }
+                    }
+                    else
+                    {
+                        Notify("Watering Can is null!", true); 
+                    }
                 }
             }
             else if (crop.FullyGrown)
             {
                 if (Plugin.EnableAutoScythe.Value)
                 {
-                    HandleToolInteraction(ScytheIndex, "No scythe on action bar!");
+                    var scytheToolData = GetToolDataByToolIndex(ScytheIndex);
+                    if (scytheToolData is not null)
+                    {
+                        if (CanUse(scytheToolData))
+                        {
+                            HandleToolInteraction(ScytheIndex, "No scythe on action bar!");   
+                        }
+                        else
+                        {
+                            Notify($"Your {scytheToolData.profession} level is too low to use {scytheToolData.name}!",true);
+                        }
+                    }
+                    else
+                    {
+                        Notify("Scythe is null!", true); 
+                    }
                 }
             }
         }
-        else if (Vector2.Distance(Player.Instance.ExactGraphicsPosition, Wish.Utilities.MousePositionFloat()) < 20 &&
-                 SingletonBehaviour<GameManager>.Instance.HasWater(new Vector2Int((int) Wish.Utilities.MousePositionFloat().x, (int) Wish.Utilities.MousePositionFloat().y)))
+        else if (Vector2.Distance(Player.Instance.ExactGraphicsPosition, Utilities.MousePositionFloat()) < 20 &&
+                 SingletonBehaviour<GameManager>.Instance.HasWater(new Vector2Int((int) Utilities.MousePositionFloat().x, (int) Utilities.MousePositionFloat().y)))
         {
             if (Plugin.EnableAutoFishingRod.Value)
             {
-                HandleToolInteraction(FishingRodIndex, "No fishing rod on action bar!");
+                var rodToolData = GetToolDataByToolIndex(FishingRodIndex);
+                if (rodToolData is not null)
+                {
+                    if (CanUse(rodToolData))
+                    {
+                        HandleToolInteraction(ScytheIndex, "No fishing rod on action bar!");   
+                    }
+                    else
+                    {
+                        Notify($"Your {rodToolData.profession} level is too low to use {rodToolData.name}!",true);
+                    }
+                }
+                else
+                {
+                    Notify("Fishing Rod is null!", true); 
+                }
+            }
+        }
+    }
+    
+    private static void ToolAction(int tool, bool condition, bool pluginValue, string errorMessage)
+    {
+        if (condition && pluginValue)
+        {
+            var toolData = GetToolDataByToolIndex(tool);
+            if (toolData != null)
+            {
+                if (CanUse(toolData))
+                {
+                    HandleToolInteraction(tool, errorMessage);
+                }
+                else
+                {
+                    Notify($"Your {toolData.profession} level is too low to use {toolData.name}!", true);
+                }
+            }
+            else
+            {
+                Notify($"{tool} is null!", true);
             }
         }
     }
 
-    // private static bool CanUse(ToolData toolData)
-    // {
-    //     return SingletonBehaviour<GameSave>.Instance.CurrentSave.characterData.Professions[toolData.profession].level >= toolData.requiredLevel;
-    // }
+    private static bool CanUse(ToolData toolData)
+    {
+        return SingletonBehaviour<GameSave>.Instance.CurrentSave.characterData.Professions[toolData.profession].level >= toolData.requiredLevel;
+    }
 
     private static void Notify(string message, bool error = false)
     {
--- AutoTools/Plugin.cs
+++ AutoTools/Plugin.cs
@@ -13,7 +13,7 @@ public class Plugin : BaseUnityPlugin
     private const string PluginGuid = "p1xel8ted.sunhaven.autotools";
     private const string PluginName = "AutoTools";
     private const string PluginVersion = "0.0.1";
-    internal static ManualLogSource LOG { get; set; }
+    internal static ManualLogSource LOG { get; private set; }
     
     internal static ConfigEntry<bool> EnableAutoTool { get; private set; }
     internal static ConfigEntry<bool> EnableAutoToolOnFarmTiles { get; private set; }
@@ -21,11 +21,10 @@ public class Plugin : BaseUnityPlugin
     internal static ConfigEntry<bool> EnableAutoAxe { get; private set; }
     internal static ConfigEntry<bool> EnableAutoScythe { get; private set; }
     internal static ConfigEntry<bool> EnableAutoFishingRod { get; private set; }
-   // internal static ConfigEntry<int> AutoFishingRodDistance { get; private set; }
     internal static ConfigEntry<bool> EnableAutoWateringCan { get; private set; }
     internal static ConfigEntry<bool> EnableAutoSword { get; private set; }
     internal static ConfigEntry<bool> EnableAutoHoe { get; private set; }
-    
+    internal static ConfigEntry<bool> EnableDebug { get; private set; }
 
     private void Awake()
     {
@@ -39,11 +38,10 @@ public class Plugin : BaseUnityPlugin
         EnableAutoAxe = Config.Bind("02. Specific Tools", "Enable AutoAxe", true, new ConfigDescription("Enable AutoAxe.", null, new ConfigurationManagerAttributes {Order = 23}));
         EnableAutoScythe = Config.Bind("02. Specific Tools", "Enable AutoScythe", true, new ConfigDescription("Enable AutoScythe.", null, new ConfigurationManagerAttributes {Order = 22}));
         EnableAutoFishingRod = Config.Bind("02. Specific Tools", "Enable AutoFishingRod", true, new ConfigDescription("Enable AutoFishingRod.", null, new ConfigurationManagerAttributes {Order = 21}));
-        //AutoFishingRodDistance = Config.Bind("01. General", "AutoFishingRodDistance", 20, new ConfigDescription("Distance to water to switch.", new AcceptableValueRange<int>(1,20), new ConfigurationManagerAttributes {Order = 20}));
         EnableAutoWateringCan = Config.Bind("02. Specific Tools", "Enable AutoWateringCan", true, new ConfigDescription("Enable AutoWateringCan.", null, new ConfigurationManagerAttributes {Order = 19}));
         EnableAutoSword = Config.Bind("02. Specific Tools", "Enable AutoSword", true, new ConfigDescription("Enable AutoSword.", null, new ConfigurationManagerAttributes {Order = 18}));
         EnableAutoHoe = Config.Bind("02. Specific Tools", "Enable AutoHoe", true, new ConfigDescription("Enable AutoHoe.", null, new ConfigurationManagerAttributes {Order = 17}));
-        
+        EnableDebug = Config.Bind("03. Debug", "Enable Debug", false, new ConfigDescription("Enable Debug.", null, new ConfigurationManagerAttributes {Order = 16}));
         
         Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginGuid);
         LOG.LogWarning($"Plugin {PluginName} is loaded!");
