90ce1aee1d49340a5495fb2f6321424472574e0c
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-10-29 01:52:45 +1100

    More Scythes Redux

---
+++ MoreScythesRedux/Patches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Wish;
+
+namespace MoreScythesRedux;
+
+[Harmony]
+public static class Patches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MainMenuController), nameof(MainMenuController.Awake))]
+    public static void MainMenuControllerAwake()
+    {
+        ItemHandler.CreateScytheItems();
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(UseItem), nameof(UseItem.SetPlayer))]
+    public static void UseItemSetPlayer(ref UseItem __instance)
+    {
+        if (!__instance.gameObject.activeSelf)
+        {
+            __instance.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ MoreScythesRedux/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Wish;
+
+namespace MoreScythesRedux;
+
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public class Plugin : BaseUnityPlugin
+{
+    private const string PluginGuid = "p1xel8ted.sunhaven.morescythesredux";
+    private const string PluginName = "More Scythes Redux";
+    private const string PluginVersion = "0.1.0";
+    public static ManualLogSource Log { get; private set; }
+
+    private void Awake()
+    {
+        Log = Logger;
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginGuid);
+        Logger.LogInfo($"{PluginName} plugin has loaded successfully.");
+    }
+
+
+}
